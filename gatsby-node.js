200bbb921f05bbc2cb378a902e36b914cbab8796
biszczak <dbiszczak@gmail.com>
2020-09-28 23:25:40 +0200

    fix window variable problem when site is building

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,19 @@
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
+
+
 const _ = require(`lodash`)
 const Promise = require(`bluebird`)
 const path = require(`path`)
